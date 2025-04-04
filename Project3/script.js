eb9cf63e81d0030298655c493384c89983e82d9e
Rishi123Githhub <sreenivasasirisharapuru@gmail.com>
2025-04-04 13:29:53 +0530

    Add files via upload

---
+++ Project3/script.js
@@ -0,0 +1,11 @@
+//function to validate registration form
+function validate registration () {
+    const username=document.get element by id('user name')values:
+    const password=document.get element by id('password')values:
+    if(use name.trim()==="//password trim ()==="){
+        alert("please enter both username and password");
+        return false;
+    }
+    //Additional validation logic can be added as needed
+    return true;
+}
